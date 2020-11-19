f6d5a5766f69c9bf0580806b02893e5f66b5d376
tommytim0515 <xtianae@connect.ust.hk>
2020-11-19 11:42:02 +0800

    SD card SDIO setup.

--- Inc/sdio.h
+++ Inc/sdio.h
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * File Name          : USART.h
+  * File Name          : SDIO.h
   * Description        : This file provides code for the configuration
-  *                      of the USART instances.
+  *                      of the SDIO instances.
   ******************************************************************************
   * @attention
   *
@@ -17,8 +17,8 @@
   ******************************************************************************
   */
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __usart_H
-#define __usart_H
+#ifndef __sdio_H
+#define __sdio_H
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -30,15 +30,13 @@
 
 /* USER CODE END Includes */
 
-extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart3;
+extern SD_HandleTypeDef hsd;
 
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
 
-void MX_USART1_UART_Init(void);
-void MX_USART3_UART_Init(void);
+void MX_SDIO_SD_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
@@ -47,7 +45,7 @@ void MX_USART3_UART_Init(void);
 #ifdef __cplusplus
 }
 #endif
-#endif /*__ usart_H */
+#endif /*__ sdio_H */
 
 /**
   * @}
